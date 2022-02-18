ea035eb4bb8eb983a6be3050611bec350fcde0c8
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-18 20:34:50 +0000

    Added code to provide exception handling to the API enabling appropriate messages to be displayed when bad or incorrect requests are made by the user.

--- src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
+++ src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
@@ -7,8 +7,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import com.techreturners.bookmanager.exception.ApiRequestException;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/v1/book")
@@ -25,30 +29,44 @@ public class BookManagerController {
 
     @GetMapping({"/{bookId}"})
     public ResponseEntity<Book> getBookById(@PathVariable Long bookId) {
-        return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            throw new ApiRequestException("Oops, Requested book not found");
+        }
     }
 
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
-        Book newBook = bookManagerService.insertBook(book);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("book", "/api/v1/book/" + newBook.getId().toString());
-        return new ResponseEntity<>(newBook, httpHeaders, HttpStatus.CREATED);
+        try {
+            Book newBook = bookManagerService.insertBook(book);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("book", "/api/v1/book/" + newBook.getId().toString());
+            return new ResponseEntity<>(newBook, httpHeaders, HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new ApiRequestException("Input Json is not valid");
+        }
     }
 
     //User Story 4 - Update Book By Id Solution
     @PutMapping({"/{bookId}"})
     public ResponseEntity<Book> updateBookById(@PathVariable("bookId") Long bookId, @RequestBody Book book) {
-        bookManagerService.updateBookById(bookId, book);
-        return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        try {
+            bookManagerService.updateBookById(bookId, book);
+            return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            throw new ApiRequestException("Oops, Book to be updated not found");
+        }
     }
 
 
     @DeleteMapping({"delete/{bookId}"})
-    public ResponseEntity<Book> deleteBookById(@PathVariable("bookId") Long bookId){
-        bookManagerService.deleteBookById(bookId);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Book> deleteBookById(@PathVariable("bookId") Long bookId) {
+        try {
+            bookManagerService.deleteBookById(bookId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ApiRequestException("Oops, Book to be deleted not found");
+        }
     }
-
-
 }
